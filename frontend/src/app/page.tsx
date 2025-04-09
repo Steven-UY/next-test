aa08886f5a8fb34b0b9cf88774715e9cc7c0ffef
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2025-04-09 01:29:14 -0400

    pull image put cardList in its own div

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,9 +5,11 @@ import CardList from "@/components/CardList";
 
 export default function Page() {
   return(
-    <div className="flex flex-col items-center min-h-screen">
-      <h1 className="text-4xl font-bold mt-8 pt-4">Pokédex Remastered</h1>
-      <div className="flex-grow">
+    <div className="flex flex-col min-h-screen">
+      <div className= "flex justify-center items-center">
+        <h1 className="text-4xl font-bold mt-8 pt-4">Pokédex Remastered</h1>
+      </div>
+      <div className="flex-grow px-6 md:px-12">
         <CardList></CardList>
       </div>
     </div>
--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -3,8 +3,10 @@ import { useState, useEffect} from "react"
 
 interface pokemon{
     name:string;
+    sprites:{
+        front_default:string;
+    }
 }
-
 
 export default function CardList() {
     const [pokemon, setPokemon] = useState<pokemon | null>(null);
@@ -17,14 +19,16 @@ export default function CardList() {
           })
           .then(data => {
             setPokemon(data);
-
           })
     }, []);
 
     return(
-        pokemon &&(<div className ="grid grid-cols-3 gap-10">
+        pokemon &&(<div className ="grid grid-cols-4 gap-12">
                 <Card>
-                    <CardTitle>{pokemon.name}</CardTitle>
+                  <div>
+                    <img src={pokemon.sprites.front_default}/>
+                  </div>
+                  <CardTitle>{pokemon.name}</CardTitle>
                 </Card>
             </div>
     )
