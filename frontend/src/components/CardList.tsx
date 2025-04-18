9c9eb2eed18059362ce5e05c5a9f75078133586d
Steven Uy <81347262+Steven-UY@users.noreply.github.com>
2025-04-18 18:58:50 -0400

    converted useEffect functions to use async and await keywords

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -19,30 +19,32 @@ export default function CardList() {
     const [details, setDetails] = useState<details[]>([]);
 
 
-    useEffect(() => {
-      // Fetch the Pokemon data from the API    
-        fetch("https://pokeapi.co/api/v2/pokemon/")
-          .then(res => {
-            return res.json();
-          })
-          .then(data => {
-            setPokemons(data.results);
-            console.log('Pokemon data:', data);
-          })
+    useEffect(() => { 
+      // Fetch the pokemon from the API
+      async function fetchPokemons() {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        const data = await response.json()
+        setPokemons(data.results);
+      }
+      fetchPokemons();
     }, []);
 
     useEffect(() => {
-      const promises = pokemons.map(pokemon => {
-        return fetch(pokemon.url)
-         .then(res => res.json());
-      });
+      const fetchDetails = async () => {
+        const pokemonDetailsPromises = pokemons.map(async (pokemon) => {
+          const response = await fetch(pokemon.url);
+          return await response.json();
+        });
 
-      Promise.all(promises).then(allPokemonData => {
+        const allPokemonData = await Promise.all(pokemonDetailsPromises);
+
         setDetails(allPokemonData);
-      })
+      };
+      if (pokemons.length > 0) {
+        fetchDetails();
+      }
     }, [pokemons]);
-
-
+    
     const pokemonList = details.map(pokemonDetails => (
       <Card key={pokemonDetails.name} className="overflow-hidden hover:shadow-lg transition-shadow">
         <div className="p-4 flex justify-center bg-gray-50">
